732e518aa4aed6f6eda4e0077f21c35401faefb5
siemsonNarson <adam.from.fishbane@gmail.com>
2020-06-22 14:59:41 +0200

    added widget

---
+++ Block/Widget/Phone.php
@@ -0,0 +1,11 @@
+<?php
+namespace Forter\Forter\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Phone extends Template implements BlockInterface {
+
+    protected $_template = "widget/phone.phtml";
+
+}
