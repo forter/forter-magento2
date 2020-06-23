80dc1668eefa03c655955935d4a2e5ac22966565
siemsonNarson <adam.from.fishbane@gmail.com>
2020-06-23 17:40:02 +0200

    updated isEnabled mode and added fonts dependencies

--- Block/Widget/Phone.php
+++ Block/Widget/Phone.php
@@ -2,10 +2,52 @@
 namespace Forter\Forter\Block\Widget;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 use Magento\Widget\Block\BlockInterface;
 
 class Phone extends Template implements BlockInterface {
 
+    /**
+     *
+     */
+    const XML_IS_ENABLED = 'forter/settings/enabled';
+
+    /**
+     * @var string
+     */
     protected $_template = "widget/phone.phtml";
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * Constructor
+     *
+     * @param Template\Context $context
+     * @param array $data
+     */
+    public function __construct(Template\Context $context, array $data = [], \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        $this->validator = $context->getValidator();
+        $this->resolver = $context->getResolver();
+        $this->_filesystem = $context->getFilesystem();
+        $this->templateEnginePool = $context->getEnginePool();
+        $this->_storeManager = $context->getStoreManager();
+        $this->_appState = $context->getAppState();
+        $this->templateContext = $this;
+        $this->pageConfig = $context->getPageConfig();
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context, $data);
+    }
+
+    public function isEnabled()
+    {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $isEnabled = $this->scopeConfig->getValue(self::XML_IS_ENABLED, $storeScope);
+
+        return $isEnabled;
+    }
 
 }
--- Model/Config/Source/FontSelect.php
+++ Model/Config/Source/FontSelect.php
@@ -7,21 +7,16 @@ class FontSelect implements \Magento\Framework\Option\ArrayInterface
     public function toOptionArray()
     {
         return [
-            ['value' => 'Arial', 'label' => __('Arial')],
             ['value' => 'Roboto', 'label' => __('Roboto')],
-            ['value' => 'Times New Roman', 'label' => __('Times New Roman')],
-            ['value' => 'Times', 'label' => __('Times')],
-            ['value' => 'Courier New', 'label' => __('Courier New')],
-            ['value' => 'Courier', 'label' => __('Courier')],
-            ['value' => 'Verdana', 'label' => __('Verdana')],
-            ['value' => 'Georgia', 'label' => __('Georgia')],
-            ['value' => 'Palatino', 'label' => __('Palatino')],
-            ['value' => 'Garamond', 'label' => __('Garamond')],
-            ['value' => 'Bookman', 'label' => __('Bookman')],
-            ['value' => 'Comic Sans MS', 'label' => __('Comic Sans MS')],
-            ['value' => 'Candara', 'label' => __('Candara')],
-            ['value' => 'Arial Black', 'label' => __('Arial Black')],
-            ['value' => 'Impact', 'label' => __('Impact')]
+            ['value' => 'Old+Times+American', 'label' => __('Old Times American')],
+            ['value' => 'Courier+Prime', 'label' => __('Courier Prime')],
+            ['value' => 'Average', 'label' => __('Average')],
+            ['value' => 'Palanquin', 'label' => __('Palanquin')],
+            ['value' => 'Cormorant+Garamond', 'label' => __('Cormorant Garamond')],
+            ['value' => 'EB+Garamond', 'label' => __('EB Garamond')],
+            ['value' => 'Comic+Sans+MS', 'label' => __('Comic Sans MS')],
+            ['value' => 'Comic+Neue', 'label' => __('Comic Neue')],
+            ['value' => 'Impact', 'label' => __('Impact')],
         ];
     }
 }
