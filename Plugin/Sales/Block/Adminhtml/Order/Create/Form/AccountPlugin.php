11eea85cb38ac832024615f0669f656f28d607fd
“zach@girit.biz” <zach@girit.biz>
2020-10-07 17:04:57 +0300

    Forter

--- Plugin/Sales/Block/Adminhtml/Order/Create/Form/AccountPlugin.php
+++ Plugin/Sales/Block/Adminhtml/Order/Create/Form/AccountPlugin.php
@@ -32,10 +32,10 @@ class AccountPlugin
     {
         if ($this->isPhoneOrderEnabled()) {
             $newBlockHtml = $subject->getLayout()->createBlock('\Magento\Framework\View\Element\Template')->setTemplate('Forter_Forter::order/view/account.phtml')->toHtml();
+
             return $html . $newBlockHtml;
-        } else {
-            return $html;
         }
+        return $html;
     }
 
     /**
