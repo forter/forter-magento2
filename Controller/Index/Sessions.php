4b3c9443215b0c1b15a9e9ab96ad0efe4f3b54f1
“zach@girit.biz” <zach@girit.biz>
2021-01-04 15:29:47 +0200

    JS solution for forter token

---
+++ Controller/Index/Sessions.php
@@ -0,0 +1,37 @@
+<?php
+namespace Forter\Forter\Controller\Index;
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Validations
+ * @package Forter\Forter\Controller\Api
+ */
+class Sessions extends \Magento\Framework\App\Action\Action
+{
+
+    /**
+     * @param \Magento\Framework\App\Action\Context
+     * @param \Magento\Framework\View\Result\PageFactory
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        PageFactory $pageFactory
+    ) {
+        $this->_pageFactory = $pageFactory;
+        $this->customerSession = $customerSession;
+        return parent::__construct($context);
+    }
+
+    /**
+     *
+     */
+    public function execute()
+    {
+        $post = $this->getRequest()->getPostValue();
+        $this->customerSession->setForterToken($post['forter_token']);
+    }
+}
