3c1ee02bbd2381e7b857307627e8a27c658dd28a
“zach@girit.biz” <zach@girit.biz>
2021-01-07 09:42:28 +0200

    FUN-374 rounding

--- Controller/Index/Sessions.php
+++ Controller/Index/Sessions.php
@@ -1,6 +1,7 @@
 <?php
 namespace Forter\Forter\Controller\Index;
 
+use Forter\Forter\Model\AbstractApi;
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
@@ -11,18 +12,24 @@ use Magento\Framework\View\Result\PageFactory;
  */
 class Sessions extends \Magento\Framework\App\Action\Action
 {
+    /**
+      * @var AbstractApi
+      */
+    protected $abstractApi;
 
     /**
      * @param \Magento\Framework\App\Action\Context
      * @param \Magento\Framework\View\Result\PageFactory
      */
     public function __construct(
+        AbstractApi $abstractApi,
         Context $context,
         Session $customerSession,
         PageFactory $pageFactory
     ) {
         $this->_pageFactory = $pageFactory;
         $this->customerSession = $customerSession;
+        $this->abstractApi = $abstractApi;
         return parent::__construct($context);
     }
 
@@ -31,7 +38,11 @@ class Sessions extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        $post = $this->getRequest()->getPostValue();
-        $this->customerSession->setForterToken($post['token']);
+        try {
+            $forterToken = $this->getRequest()->getHeader('Forter-Token');
+            $this->customerSession->setForterToken($forterToken);
+        } catch (Exception $e) {
+            $this->abstractApi->reportToForterOnCatch($e);
+        }
     }
 }
