1e5c2298b5269d5a8c7cee821c114240491f3210
Sivan Wolberg <sivan@wolberg.pro>
2022-03-21 21:16:11 +0200

    add one more payment

---
+++ Model/Mapper/PaymentTypes/mercadopago.payment.php
@@ -0,0 +1,58 @@
+<?
+
+namespace Forter\Forter\Model\Mapper\PaymentTypes;
+
+class MercadopagoPayment extends BasePayment
+{
+    public function __construct()
+    {
+    }
+    public function process($order, $payment)
+    {
+        $paymentMapping = $this->mapper->payments->mercadopago;
+        $detailsArray =[];
+
+        $ccType = $payment->getAdditionalInformation($paymentMapping->cardType);
+        if ($ccType) {
+            $detailsArray['cardBrand'] = $ccType;
+        }
+
+        $expirationMonth = $payment->getAdditionalInformation($paymentMapping->cardMonth);
+        if ($expirationMonth) {
+            $detailsArray['expirationMonth'] = $expirationMonth;
+        }
+
+        $expirationYear = $payment->getAdditionalInformation($paymentMapping->cardYear);
+        if ($expirationYear) {
+            $detailsArray['expirationYear'] = $expirationYear;
+        }
+
+        $nameOnCard = $payment->getAdditionalInformation($paymentMapping->cardName);
+        if ($nameOnCard) {
+            $detailsArray['nameOnCard'] = $nameOnCard;
+        }
+
+        $continuerMapping = $paymentMapping->paymentMeta;
+        $mercadoPayment = $payment->getAdditionalInformation($continuerMapping->continuer);
+
+        if ($mercadoPayment) {
+            if (isset($mercadoPayment[$continuerMapping->authCode])) {
+                $detailsArray['authorizationCode'] = $mercadoPayment[$continuerMapping->authCode];
+            }
+
+            if (isset($mercadoPayment[$continuerMapping->cardInfo][$continuerMapping->cardLast6Dig])) {
+                $detailsArray['bin'] = $mercadoPayment[$continuerMapping->cardInfo][$continuerMapping->cardInfo];
+            }
+
+            if (isset($mercadoPayment[$continuerMapping->cardInfo][$continuerMapping->cardLast4Dig])) {
+                $detailsArray['lastFourDigits'] = $mercadoPayment[$continuerMapping->cardInfo]['last_four_digits'];
+            }
+        }
+
+        return $this->preferCcDetails($payment, $detailsArray);
+    }
+    public function getExtraData($order, $payment)
+    {
+        return null;
+    }
+}
