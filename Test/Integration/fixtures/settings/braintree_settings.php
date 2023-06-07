f0e18fd5516ad95c0b014b5ba21544dd2fa1172c
Cristian Balaj <cristian.balaj@osf.digital>
2023-06-07 09:40:46 +0300

    Added integrations tests.

--- Test/Integration/fixtures/settings/braintree_settings.php
+++ Test/Integration/fixtures/settings/braintree_settings.php
@@ -1,7 +1,8 @@
 <?php
+
 $braintreeSettings = [
-    'sandbox_merchant_id' => 't6zvx2js3gt4jj85',
-    'sandbox_public_key' => 't6v2y24h7twb9zyp',
-    'sandbox_private_key' => '754181df1e828fb25be93e6548e4bcf5',
+    'sandbox_merchant_id' => 'YOUR_SANDBOX_MERCHANT_ID',
+    'sandbox_public_key' => 'YOUR_SANDBOX_PUBLIC_KEY',
+    'sandbox_private_key' => 'YOUR_SANDBOX_PRIVATE_KEY',
 ];
 
--- Test/Integration/fixtures/settings/forter_settings.php
+++ Test/Integration/fixtures/settings/forter_settings.php
@@ -1,7 +1,7 @@
 <?php
-$forterSettings = [
-    'site_id' => '194d494d2557',
-    'secret_key' => '30a0af07fe3414b731c094690223fa74927d7e17',
-    'api_version' => '2.17'
-];
 
+$forterSettings = [
+    'site_id' => 'YOUR_SITE_ID',
+    'secret_key' => 'YOUR_SECRET_KEY',
+    'api_version' => 'PREFERED_API_VERSION'
+];
