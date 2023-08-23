14859277414d82c5b17c204c4eb88c52874af1c6
ioancatanoaiaosf <57447552+ioancatanoaiaosf@users.noreply.github.com>
2023-08-23 20:42:23 +0300

    Release/2.4.16 (#115)
    * OSF-22 Change default value of enhanced data mode to false.
    
    * OSF-21 Added reason code mapping.
    
    * OSF-21 Added reason code mapping.
    
    * OSF-26 Close the loading mask
    
    * Added GPay
    
    * Version bump
    
    * code optimization
    
    ---------
    
    Co-authored-by: Cristian Balaj <cristian.balaj@osf.digital>

---
+++ Observer/ThirdParty/Adyen/AdyenHppDataAssignObserver.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Forter\Forter\Observer\ThirdParty\Adyen;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Module\Manager as ModuleManager;
+use Magento\Payment\Observer\AbstractDataAssignObserver;
+use Magento\Quote\Api\Data\PaymentInterface;
+
+class AdyenHppDataAssignObserver extends AbstractDataAssignObserver
+{
+    const BRAND_CODE = 'brand_code';
+    const DF_VALUE = 'df_value';
+    const GUEST_EMAIL = 'guestEmail';
+    const FORTER_STATE_DATA = 'forterData';
+    const STATE_DATA = 'stateData';
+    const RETURN_URL = 'returnUrl';
+    const RECURRING_PROCESSING_MODEL = 'recurringProcessingModel';
+
+    /**
+     * Approved root level keys from additional data array
+     *
+     * @var array
+     */
+    private static $approvedAdditionalDataKeys = [
+        self::BRAND_CODE,
+        self::DF_VALUE,
+        self::GUEST_EMAIL,
+        self::STATE_DATA,
+        self::RETURN_URL,
+        self::RECURRING_PROCESSING_MODEL
+    ];
+
+    /**
+     * @var ModuleManager
+     */
+    protected $moduleManager;
+
+    /**
+     * @param ModuleManager $moduleManager
+     */
+    public function __construct(
+        ModuleManager $moduleManager
+    ) {
+        $this->moduleManager = $moduleManager;
+    }
+
+    /**
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        if (!$this->moduleManager->isEnabled('Adyen_Payment')) {
+            return null;
+        }
+
+        $data = $this->readDataArgument($observer);
+        $paymentInfo = $this->readPaymentModelArgument($observer);
+
+        $additionalData = $data->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
+        if (!is_array($additionalData)) {
+            return;
+        }
+
+        $additionalData = \Adyen\Service\Validator\DataArrayValidator::getArrayOnlyWithApprovedKeys(
+            $additionalData,
+            self::$approvedAdditionalDataKeys
+        );
+
+        if (!empty($additionalData[self::STATE_DATA])) {
+            $stateData = json_decode($additionalData[self::STATE_DATA], true);
+            $paymentInfo->setAdditionalInformation(self::FORTER_STATE_DATA, $stateData);
+        }
+    }
+}
