7685d2c929d8fd5e8a3c1a62229e3cc8fe7f4236
Ioan Catanoaia <ioan.catanoaia@osf-global.com>
2023-10-27 17:51:20 +0300

    Cleanup forter command

--- Console/Command/Decline.php
+++ Console/Command/Decline.php
@@ -2,19 +2,13 @@
 
 namespace Forter\Forter\Console\Command;
 
-use Magento\Framework\App\Area;
 use Forter\Forter\Cron\SendQueue as Cron;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Magento\Framework\App\State;
 
 class Decline extends Command
 {
-    /**
-     * @var State
-     */
-    protected $state;
 
     /**
      * @var Cron
@@ -22,17 +16,14 @@ class Decline extends Command
     protected $cron;
 
     /**
-     * @param State $state
      * @param Cron $cron
      * @param string|null $name
      */
     public function __construct(
-        State $state,
         Cron $cron,
         string $name = null
     ) {
         $this->cron = $cron;
-        $this->state = $state;
         parent::__construct($name);
     }
 
@@ -52,9 +43,7 @@ class Decline extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->state->setAreaCode(Area::AREA_ADMINHTML);
         $this->cron->execute();
-
         return 0;
     }
 }
