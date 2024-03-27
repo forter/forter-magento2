9e2587781c109602df102f592d6992bc17acac47
Cristian Balaj <cristian.balaj@osf.digital>
2024-03-27 11:23:20 +0200

    OSF-77 -removed orderFLow endpoint requests

--- Model/ForterLogger.php
+++ Model/ForterLogger.php
@@ -29,18 +29,18 @@ class ForterLogger
     public function SendLog(ForterLoggerMessage $data) {
         try {
             $json = $data->ToJson();
-            $requestOps = [];
-            $requestOps['x-forter-siteid'] = $this->forterConfig->getSiteId();
-            $requestOps['api-version'] = $this->forterConfig->getApiVersion();
-            $requestOps['x-forter-extver'] = $this->forterConfig->getModuleVersion();
-            $requestOps['x-forter-client'] = $this->forterConfig->getMagentoFullVersion();
-            $requestOps['Content-Type'] = 'application/json';
-            $requestOps['Accept'] = 'application/json';
-            $requestOps['Authorization'] =['Basic '.$this->forterConfig->getSecretKey().':'];
-            $requestOps['json'] = $json;
+//            $requestOps = [];
+//            $requestOps['x-forter-siteid'] = $this->forterConfig->getSiteId();
+//            $requestOps['api-version'] = $this->forterConfig->getApiVersion();
+//            $requestOps['x-forter-extver'] = $this->forterConfig->getModuleVersion();
+//            $requestOps['x-forter-client'] = $this->forterConfig->getMagentoFullVersion();
+//            $requestOps['Content-Type'] = 'application/json';
+//            $requestOps['Accept'] = 'application/json';
+//            $requestOps['Authorization'] =['Basic '.$this->forterConfig->getSecretKey().':'];
+//            $requestOps['json'] = $json;
             $this->forterConfig->log('send log request: ' .$this->BASE_URL.'/magento/orderFlow/'.$this->forterConfig->getSiteId().'/'.$data->orderId);
-            $response = $this->httpClient->request('post','/magento/orderFlow/'.$this->forterConfig->getSiteId().'/'.$data->orderId, $requestOps);
-            $this->forterConfig->log(sprintf('send log status: %s', $response->getStatusCode()));
+//            $response = $this->httpClient->request('post','/magento/orderFlow/'.$this->forterConfig->getSiteId().'/'.$data->orderId, $requestOps);
+//            $this->forterConfig->log(sprintf('send log status: %s', $response->getStatusCode()));
         } catch (\Exception $e) {
             $this->forterConfig->log('Error:' . $e->getMessage());
         }
